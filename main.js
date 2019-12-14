e0ec0ee3f30f84d92537671ac9f1b147868aded5
Olivier Goulet <oliviergoulet5@gmail.com>
2019-12-14 18:10:40 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,113 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron; // destructuring
+
+let mainWindow;
+let addCryptoWindow;
+
+// Listen for the app to be ready
+app.on('ready', () => {
+    // Create new window
+    mainWindow = new BrowserWindow({ 
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // Load HTML file into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Quit app when close
+    mainWindow.on('closed', () => { app.quit() });
+
+    // Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// Add Cryptocurrency Window
+function createAddCrypto() {
+    addCryptoWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Cryptocurrency',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    addCryptoWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addCryptoWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Garbage collection handling
+    addCryptoWindow.on('close', () => { addWindow = null });
+}
+
+// Catch crypto-item:add
+ipcMain.on('crypto-item:add', (event, cryptoItem) => {
+    console.log(cryptoItem);
+    mainWindow.webContents.send('crypto-item:add', cryptoItem);
+    addCryptoWindow.close();
+});
+
+// Create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Crypto',
+                accelerator: 'CmdOrCtrl+E',
+                click() {
+                    createAddCrypto();
+                }
+            },
+            {
+                label: 'Remove Crypto',
+                accelerator: 'CmdOrCtrl+T'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'CmdOrCtrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// For macOS' menu
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({});
+}
+
+// Add dev tools if not in production
+if (process.env.NODE_ENV != 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle Dev Tools',
+                accelerator: 'CmdorCtrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
